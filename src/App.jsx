af70aafe38b8ec70ae9c3fc67caf804ed22628db
L1verly <artemartem7975@gmail.com>
2026-01-31 10:33:12 +0100

    App routes setup for login, pricing, product, pagenav implementation. Added data and assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
+import "./index.css";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 export default function App() {
   return (
     <BrowserRouter>
@@ -10,6 +13,8 @@ export default function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
