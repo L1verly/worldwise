8cf30a3f5f98b3c0af0449fe63f0fea8ed9af43d
L1verly <artemartem7975@gmail.com>
2026-02-02 11:35:49 +0100

    building app layout

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer";
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
