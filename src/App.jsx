e2f37c2ac06f5f65035967ffc745fe45c0d379f5
L1verly <artemartem7975@gmail.com>
2026-02-02 12:34:27 +0100

    cities list

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,29 @@ import PageNotFound from "./pages/PageNotFound";
 import "./index.css";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:8000";
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("There was an error in loading data");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -15,7 +37,10 @@ export default function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
           <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="countries" element={<p>Lsit of countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
---
+++ src/components/CityList.jsx
@@ -0,0 +1,20 @@
+import CityItem from "./CityItem";
+import styles from "./CityList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+export default function CityList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Click a city on the map to add your first city!" />
+    );
+  return (
+    <ul className={styles.cityList}>
+      {cities.map((city) => (
+        <CityItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
