5c528081f9b94f24630b3ade90127583a2455eb1
L1verly <artemartem7975@gmail.com>
2026-02-02 11:47:44 +0100

    nested routes and navlinks for cities and countries

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import Footer from "./Footer";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import styles from "./Sidebar.module.css";
+import { Outlet } from "react-router-dom";
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <Footer />
     </div>
   );
